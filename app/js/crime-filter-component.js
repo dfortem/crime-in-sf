61c47a2cff0b45db63516cf67959546511364375
Daniel Forte Marques <daniel.fortemarques@epfl.ch>
2016-12-21 16:41:50 +0100

    Filter results according to date and crime type

--- app/js/crime-filter-component.js
+++ app/js/crime-filter-component.js
@@ -1,40 +1,124 @@
 'use strict';
 
-var CrimeController =function (crimeFactory, $filter) {
+var CrimeController = function (crimeFactory, $filter) {
   var ctrl = this;
 
   ctrl.$onInit = function () {
     ctrl.crimeToDisplay = [];
-    ctrl.a = 'I DID IT!! THE BINDING IS WORKING!!';
-    ctrl.status = 'loading';
+    ctrl.loading = true;
+    ctrl.maxDate = new Date (2015, 2, 18);
+    ctrl.defaultDate = new Date (2015, 2, 11);
+    ctrl.startDate = ctrl.defaultDate;
+    ctrl.crimeTypes = [{
+      name: 'Agravated Assault',
+      tag: 'AA',
+      selected: true}, {
+      name: 'Murder',
+      tag: 'Mu',
+      selected: true}, {
+      name: 'Robbery',
+      tag: 'Ro',
+      selected: true}, {
+      name: 'Simple Assault',
+      tag: 'SA',
+      selected: true}, {
+      name: 'Arson',
+      tag: 'Ar',
+      selected: true}, {
+      name: 'Burglary',
+      tag: 'Bu',
+      selected: true}, {
+      name: 'Theft',
+      tag: 'Th',
+      selected: true}, {
+      name: 'Vandalism',
+      tag: 'Va',
+      selected: true}, {
+      name: 'Vehicle Theft',
+      tag: 'VT',
+      selected: true}, {
+      name: 'Alcohol',
+      tag: 'Al',
+      selected: true}, {
+      name: 'Distributing the Peace',
+      tag: 'DP',
+      selected: true}, {
+      name: 'Narcotics',
+      tag: 'Na',
+      selected: true}, {
+      name: 'Prostitution',
+      tag: 'Pr',
+      selected: true}];
+
     ctrl.getCrimes();
   };
 
   ctrl.getCrimes = function () {
-    var dstart = $filter('date')(new Date (2014, 8, 1), 'yyyy-MM-dd');
-    var dend = $filter('date')(new Date (2014, 8, 30), 'yyyy-MM-dd');
-    var params = {
-      dstart: dstart,
-      dend: dend
-    };
-    crimeFactory.getLastCrimes()
+    var params = {};
+    //fiter by date
+    var dstart = $filter('date')(ctrl.startDate, 'yyyy-MM-dd');
+    params.dstart = dstart ? dstart : ctrl.defaultDate;
+    var dend = $filter('date')(ctrl.endDate, 'yyyy-MM-dd');
+    if (dend) params.dend = dend;
+    //filter by type
+    var types = '';
+    _.each(ctrl.crimeTypes, function (type) {
+      if (type.selected) types += type.tag + ',';
+    });
+    types = types.substring(0, types.length - 1);
+    console.log(types);
+    if (types !== '') params.type = types;
+    //fetch data
+    ctrl.loading = true;
+    crimeFactory.getCrimesFiltered(params)
       .then(crimeFactory.createDataList)
       .then(function (response) {
         ctrl.crimeToDisplay = response;
+        ctrl.loading = false;
         console.log('Data succesfully loaded');
       })
       .catch(function (error) {
-        ctrl.status = 'Unable to load crime data' + error.status;
-        console.log(ctrl.status);
+        ctrl.loading = false;
+        var err = 'Unable to load crime data' + error.status;
+        console.log(err);
       });
   };
 
   ctrl.onClick = function () {
-    ctrl.crimeToDisplay = [];
-    console.log(ctrl);
+    console.log('loading...');
     ctrl.getCrimes();
     console.log(ctrl.a);
   };
+
+  ctrl.selectStartDate = function () {
+    if (ctrl.startDate > ctrl.endDate) {
+      ctrl.endDate = undefined;
+    }
+  };
+
+  ctrl.isChecked = function () {
+    return _.filter(ctrl.crimeTypes, function (type) {
+      return type.selected;
+    }).length === ctrl.crimeTypes.length;
+  };
+
+  ctrl.isIndeterminate = function () {
+    return !ctrl.isChecked() && _.filter(ctrl.crimeTypes, function (type) {
+      return type.selected;
+    }).length !== 0;
+  };
+
+  ctrl.toggleAll = function () {
+    var unSelelctAll = ctrl.isChecked();
+    _.each(ctrl.crimeTypes, function (type) {
+      type.selected = unSelelctAll ? false : true;
+    });
+  };
+
+  ctrl.openMenu = function ($mdOpenMenu, ev) {
+      //originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
 };
 
 angular.module('crimeApp')
--- app/js/crime-map-component.js
+++ app/js/crime-map-component.js
@@ -1,45 +1,45 @@
 'use strict';
 
 var MapController = function (NgMap) {
-	var ctrl = this;
+  var ctrl = this;
 
-	ctrl.markerPosition = function (crime) {
-		return '[' + crime.lat + ', ' + crime.lng +']';
-	};
+  ctrl.markerPosition = function (crime) {
+    return '[' + crime.lat + ', ' + crime.lng + ']';
+  };
 
-	ctrl.display = function () {
-		console.log(ctrl);
-	};
+  ctrl.display = function () {
+    console.log(ctrl);
+  };
 
-	ctrl.onClick = function (e, crime) {
-		console.log('you\'ve clicked on the marker');
-		console.log(crime);
-		ctrl.displayedCrime = crime;
+  ctrl.onClick = function (e, crime) {
+    console.log('you\'ve clicked on the marker');
+    console.log(crime);
+    ctrl.displayedCrime = crime;
     ctrl.map.showInfoWindow('info', crime.case_number);
-	};
+  };
 
-	ctrl.$onInit = function () {
-		console.log('INIT!!!');
-		NgMap.getMap().then(function(map) {
-	    console.log('map', map);
-	    ctrl.map = map;
-	  }).catch(function(err) {
-	  	console.log('ERROR!');
-	  	console.log(err);
-	  });
-		ctrl.displayedCrime = {};
-	};
+  ctrl.$onInit = function () {
+    console.log('INIT!!!');
+    NgMap.getMap().then(function (map) {
+      console.log('map', map);
+      ctrl.map = map;
+    }).catch(function (err) {
+      console.log('ERROR!');
+      console.log(err);
+    });
+    ctrl.displayedCrime = {};
+  };
 
-	ctrl.$onChanges = function (changes) {
-		ctrl.crimes = changes.crimes.currentValue;
-		console.log('UPDATE!!!!');
-	};
+  ctrl.$onChanges = function (changes) {
+    ctrl.crimes = changes.crimes.currentValue;
+    console.log('UPDATE!!!!');
+  };
 };
 
 angular.module('crimeApp').component('crimeMap', {
-	templateUrl: 'partials/crime-map.html',
-	controller: MapController,
-	bindings: {
-		crimes: '<'
-	}
+  templateUrl: 'partials/crime-map.html',
+  controller: MapController,
+  bindings: {
+    crimes: '<'
+  }
 });
